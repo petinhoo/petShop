4b7168601443f9695fcabe97e3d60304a25b7b2e
20181064110026 <20181064110026@JC384583.ifrn.local>
2019-09-19 21:39:45 -0300

    ENVIANDO ARQUIVO DE CONFIHURAÇÃO DO ECLIPSE.

--- testegithub/src/testegithub/Petinho.java
+++ testegithub/src/testegithub/Petinho.java
@@ -1,9 +1,14 @@
 package testegithub;
 
+import javax.sound.midi.Soundbank;
+
 public class Petinho {
 
 	public static void main(String[] args) {
 		System.out.println("iara, me da seu numero");
+		
+		System.out.println("OI :");
 	}
+	
 	
 }
